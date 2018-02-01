dfadaefc6f93cb8c5be20ccd4186b45a6792db1c
lz <lz>
2018-02-01 11:10:03 +0800

    优化了回调事件的

--- js/index.js
+++ js/index.js
@@ -224,18 +224,14 @@ var Leaflet;
     //动画事件
     function animationEnd(that){
         for (var i = 0; i < that.pages.length; i++) {
-            var page=that.pages[i]
-            addEvent(that,that.pages[i],"animationend",function(current,that,e){
-                if(that.bindEle!==null&&that.bindEle.current[0]===current){action(that,"end");}else{return;}
-               });
-            addEvent(that,that.pages[i],"animationstart",function(current,that,e){
-                if(that.bindEle!==null&&that.bindEle.current[0]===current){action(that,"start");}else{return;}
-               });
-            addEvent(that,that.pages[i],"webkitAnimationEnd",function(current,that,e){
-                if(that.bindEle!==null&&that.bindEle.current[0]===current){action(that,"end");}else{return;}
-              });
-              addEvent(that,that.pages[i],"webkitAnimationStart",function(current,that,e){
-                if(that.bindEle!==null&&that.bindEle.current[0]===current){action(that,"start");}else{return;}
+              bind(that,that.pages[i],"animationend","end");
+              bind(that,that.pages[i],"animationstart","start");
+              bind(that,that.pages[i],"webkitAnimationEnd","end");
+              bind(that,that.pages[i],"webkitAnimationStart","start");
+        }
+        function bind(that,ele,type,flage){
+             addEvent(that,that.pages[i],type,function(current,that,e){
+                if(that.bindEle!==null&&that.bindEle.current[0]===current){action(that,flage);}else{return;}
               });
         }
         function action(that,flage){
